54bc5396e8a86b739adbaaa3cb3623ad1be0ad4d
hailinh <hailinh.akira@gmail.com>
2017-08-14 18:39:12 +0700

    Nộp bài tập buổi 7

---
+++ Session 7/DecimaltoBinary/DecimaltoBinary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DecimaltoBinary;
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+	{
+//		1. Viết 1 chương trình convert 1 list có N phần tử (không cần nhập vào từ bàn phím) số hệ thập phân ra hệ nhị phân nhé, mấy cái 10001011 ý :)))
+
+ 	List<int> numbers = new List<int> ();
+	Random rand = new Random();
+
+	for (int i = 0; i < 11; i++) 
+	{
+		numbers.Add (System.Random(0, 100));
+		Console.WriteLine(numbers);
+
+		for (int x=0; x < numbers.Count; x++)
+		{
+			string binary = Convert.ToString(numbers[x],2);
+					Console.WriteLine("This number:" + numbers[x]);
+					Console.WriteLine(" In binary is: " + binary);
+		}
+	
+
+	}
+	}
+}
---
+++ Session 7/GCD/GCD/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GCD
+{
+	public class Program
+	{
+		public static int gcd(int a, int b)
+		{
+			if(b == 0)
+			{
+				return a;
+			}
+			else
+			{
+				return gcd(b, a % b);
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			//Your code goes here
+
+			List<int> list = new List<int>(new int[]{ 24, 8, 12 });
+			// Loop with for and use string interpolation to print values.
+			int result = list[0];
+			for (int i = 1; i < list.Count; i++)
+			{
+				result = gcd(result, list[i]);
+			}
+			Console.WriteLine(result);
+}
